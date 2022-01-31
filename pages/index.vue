d5b1376a12d3e391e6ab4ae9dc1a6a1a96f7a73b
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-02-01 01:27:37 +0600

    commit class #1

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>This is Index Page.</p>
+  </div>
+</template>
+
+<script>
+import Footer from '../components/Footer.vue';
+import Header from '../components/Header.vue';
+export default {
+  name: 'IndexPage',
+  components: { Header, Footer }
+}
+</script>
---
+++ plugins/vue-slick-carousel.js
@@ -0,0 +1,7 @@
+import Vue from "vue";
+
+
+import VueSlickCarousel from "vue-slick-carousel"
+
+
+
