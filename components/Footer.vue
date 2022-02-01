a94185dcf6096bd21adb0431bd35947d6b108cc9
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-02-01 15:53:59 +0600

    commit class #4 Footer top

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,48 @@
 <template>
   <div>
-    Footer
+    <div class="container">
+      <div class="flex -mx-4 mb-16">
+        <div class="w-1/4 px-4 flex items-center">
+          <div class="min-w-max mr-4">
+            <img width="80" src="~/assets/img/customer-support.png" alt="customer-support">
+          </div>
+          <div class="w-full">
+            <h3 class="text-xl font-medium mb-2">24 Customer Support</h3>
+            Contact us 24 hours
+          </div>
+        </div>
+
+        <div class="w-1/4 px-4 flex items-center">
+          <div class="min-w-max mr-4">
+            <img width="80" src="~/assets/img/authentic-products.png" alt="authentic-products">
+          </div>
+          <div class="w-full">
+            <h3 class="text-xl font-medium mb-2">Authentic Products</h3>
+            Contact us 24 hours
+          </div>
+        </div>
+
+        <div class="w-1/4 px-4 flex items-center">
+          <div class="min-w-max mr-4">
+            <img width="80" src="~/assets/img/secure-payment.png" alt="secure-payment">
+          </div>
+          <div class="w-full">
+            <h3 class="text-xl font-medium mb-2">Secure Payment</h3>
+            Contact us 24 hours
+          </div>
+        </div>
+
+        <div class="w-1/4 px-4 flex items-center">
+          <div class="min-w-max mr-4">
+            <img width="80" src="~/assets/img/best-price.png" alt="best-price">
+          </div>
+          <div class="w-full">
+            <h3 class="text-xl font-medium mb-2">Best Prices & Offers</h3>
+            Contact us 24 hours
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
